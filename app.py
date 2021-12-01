5254eade2f9f6f5238dfc8a14ccf72025f925f85
Arindam <arindamkeswani@gmail.com>
2021-12-01 15:59:50 +0530

    What are blueprints in Flask?

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-# from learning import learning_blueprint
+from learning import learning_blueprint
 
 
 app=Flask(__name__)
 
-# app.register_blueprint(learning_blueprint, url_prefix="/greetings") #done to make app.py access the blueprint
+app.register_blueprint(learning_blueprint, url_prefix="/greetings") #done to make app.py access the blueprint
 
 
-@app.route("/")
-def home():
-    return "Hello world"
+# @app.route("/")
+# def home():
+#     return "Hello world"
 
 if __name__ == '__main__':
     app.run()
--- learning.py
+++ learning.py
@@ -1,11 +1,11 @@
-# from flask import Blueprint
+from flask import Blueprint
 
-# learning_blueprint = Blueprint('learning', __name__)
+learning_blueprint = Blueprint('learning', __name__)
 
-# # @learning_blueprint.route("/")
-# # def home():
-# #     return "Hello world"
+# @learning_blueprint.route("/")
+# def home():
+#     return "Hello world"
 
-# @learning_blueprint.route("/<string:name>")
-# def home(name):
-#     return f"Hello, {name}"
+@learning_blueprint.route("/<string:name>")
+def home(name):
+    return f"Hello, {name}"
